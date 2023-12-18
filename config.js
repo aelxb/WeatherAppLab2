53f6cd933fa08edf6123e2a85672c1b8d222e7df
aelxb <67102261+aelxb@users.noreply.github.com>
2023-12-18 14:40:34 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap:{
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=Moscow&units=metric",
+        SECRET_KEY: "8593ca2b68bfc28ede2d2e01d2efbf62"
+    }
+}
+
+module.exports = constants
